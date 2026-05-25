0cd86adf51957cb20382c96d93485dbe3e814c89
Attitude <3415181520@qq.com>
2026-05-25 23:22:02 +0800

    通过引入config依赖和使用@RefreshScope注解实现动态加载配置中心配置规则

--- services/service-order/src/main/java/com/shuhang/controller/OrderController.java
+++ services/service-order/src/main/java/com/shuhang/controller/OrderController.java
@@ -3,14 +3,20 @@ package com.shuhang.controller;
 import com.shuhang.model.order.Order;
 import com.shuhang.service.OrderService;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope   // 实现动态刷新Nacos的配置
 @RestController
 @RequestMapping("/order")
 public class OrderController {
+
+    @Value("${order.timeout}")
+    String timeout;
 
     @Resource
     private OrderService orderService;
@@ -19,5 +25,11 @@ public class OrderController {
     public Order create(@RequestParam("productId") Long productId,
                               @RequestParam("userId") Long userId){
         return orderService.createOrder(productId,userId);
+    }
+
+
+    @GetMapping("/config")
+    public String getOrderConfig() {
+        return timeout;
     }
 }
